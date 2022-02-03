f5fb8fcbd1a5706e052a75d65738413d86906ca3
Angelo Dan F. Bagasbas <a.bagasbas@createit.com>
2022-02-03 23:34:45 +0800

    #74 - Added save custom field to database, added update function for custom field, added a shortcode/smarty value for frontend

--- modules/createit_customfield/createit_customfield.php
+++ modules/createit_customfield/createit_customfield.php
@@ -54,8 +54,10 @@ class CreateIt_CustomField extends Module
         }
 
         if (
-            parent::install() == false ||
-            !$this->registerHook('displayAdminProductsMainStepLeftColumnMiddle')
+            parent::install() == false
+            || !$this->registerHook('displayAdminProductsMainStepLeftColumnMiddle')
+            || !$this->registerHook('actionProductUpdate')
+            || !$this->registerHook('actionFrontControllerSetVariables')
         ) {
             return false;
         }
@@ -95,32 +97,119 @@ class CreateIt_CustomField extends Module
 
     public function hookDisplayAdminProductsMainStepLeftColumnMiddle($params)
     {
+        $customField = '';
+
+        if($this->isHaveCustomFieldValue($params['id_product'], Context::getContext()->shop->id, Context::getContext()->language->id)){
+
+            $currentProductCustomField = $this->getProductCustomField($params['id_product'], Context::getContext()->shop->id, Context::getContext()->language->id);
+
+            $customField = reset($currentProductCustomField);
+            $customField = $customField['content'];
+        }
+
+        $this->context->smarty->assign([
+            'createit_custom_field' => $customField
+        ]);
+
         return $this->display(__FILE__, 'views/templates/admin/products_custom_field.tpl');
     }
 
+    public function isHaveCustomFieldValue($id_product, $id_shop, $id_lang)
+    {
+        if( $this->getProductCustomField($id_product, $id_shop, $id_lang) ){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
     public function hookActionProductUpdate($params)
     {
         require_once dirname(__FILE__) . '/CreateitCustomField.php';
 
-        /**
-         * TODO
-         */
-
+        $currentDateTime= new \DateTime('now', new \DateTimeZone('UTC'));
         $id_product = (int)Tools::getValue('id_product');
-        $createit_custom_field = (int)Tools::getValue('createit_custom_field');
+        $createit_custom_field = Tools::getValue('createit_custom_field');
 
-        if($id_product && Tools::isSubmit('createit_custom_field')) {
+        $currentProductCustomField = $this->getProductCustomField($id_product, Context::getContext()->shop->id, Context::getContext()->language->id);
+
+        if(!$currentProductCustomField){
             $id = null;
             $fn = new CreateitCustomField($id);
-            $fn->id_product = 14;
-            $fn->id_shop = 1; //TODO
-            $fn->id_lang = 1;
+            $fn->id_product = $id_product;
+            $fn->id_shop = Context::getContext()->shop->id;
+            $fn->id_lang = Context::getContext()->language->id;
             $fn->content = $createit_custom_field;
-            $fn->created_at = new \DateTime('now', new \DateTimeZone('UTC'));
-            $fn->updated_at = new \DateTime('now', new \DateTimeZone('UTC'));
+            $fn->created_at = $currentDateTime->format('Y-m-d H:i:s');
+            $fn->updated_at = $currentDateTime->format('Y-m-d H:i:s');
 
             $fn->save();
+        }else{
+            $this->update($id_product, Context::getContext()->shop->id, Context::getContext()->language->id, $createit_custom_field);
         }
+
+    }
+
+    public function update($id_product, $id_shop, $id_lang, $createit_custom_field)
+    {
+        $currentDateTime= new \DateTime('now', new \DateTimeZone('UTC'));
+        $updatedDate = $currentDateTime->format('Y-m-d H:i:s');
+
+        return Db::getInstance()->execute('
+            UPDATE `' . _DB_PREFIX_ . 'createit_customfield`
+            SET `content` = \'' . pSQL($createit_custom_field) . '\' , `updated_at` = \'' . pSQL($updatedDate) . '\'
+            WHERE id_product = '. (int) $id_product .' AND
+                    id_shop = '. (int) $id_shop  .' AND
+                    id_lang = '. (int) $id_lang
+        );
+    }
+
+    public function getProductCustomField($id_product, $id_shop, $id_lang)
+    {
+        if (!Validate::isUnsignedId($id_product) ||
+            !Validate::isUnsignedId($id_lang) ||
+            !Validate::isUnsignedId($id_lang)) {
+            exit(Tools::displayError());
+        }
+        $res = Db::getInstance()->executeS('
+                    SELECT *
+                    FROM `' . _DB_PREFIX_ . 'createit_customfield`
+                    WHERE id_product = '. (int) $id_product .'
+                    AND id_shop = '. (int) $id_shop  .'
+                    AND id_lang = '. (int) $id_lang);
+        return $res;
+    }
+
+    public function getProductCustomFieldValue($id_product, $id_shop, $id_lang)
+    {
+        $customField = '';
+
+        if($this->isHaveCustomFieldValue($id_product, $id_shop, $id_lang)){
+
+            $currentProductCustomField = $this->getProductCustomField($id_product, $id_shop, $id_lang);
+
+            $customField = reset($currentProductCustomField);
+            $customField = $customField['content'];
+        }
+
+        return $customField;
+    }
+
+    public function hookActionFrontControllerSetVariables($param)
+    {
+
+        $customField = '';
+
+        if(!is_null(Tools::getValue('id_product'))){
+            /**
+             * TODO
+             */
+            /**
+            $customField = $this->getProductCustomFieldValue(Tools::getValue('id_product'), Context::getContext()->shop->id, Context::getContext()->language->id);
+             **/
+        }
+        return [
+            'custom_field' => ''
+        ];
     }
 }
--- modules/createit_customfield/views/templates/admin/products_custom_field.tpl
+++ modules/createit_customfield/views/templates/admin/products_custom_field.tpl
@@ -3,7 +3,7 @@
     <fieldset class="form-group">
         <div class="col-lg-12 col-xl-4">
             <label class="form-control-label">Custom Field</label>
-            <input class="form-control" name="createit_custom_field" type="text">
+            <input class="form-control" name="createit_custom_field" type="text" value="{$createit_custom_field}">
         </div>
     </fieldset>
     <div class="clearfix"></div>
