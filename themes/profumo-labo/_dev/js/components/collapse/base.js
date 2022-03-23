ecb3d569e7a6396cd55125a61161a8aba0c6016b
John Clark Semblante <jc.semblante@createit.com>
2022-03-23 20:18:20 +0800

    120: Update collapse link to include langauge switcher link update

--- themes/profumo-labo/_dev/js/components/collapse/base.js
+++ themes/profumo-labo/_dev/js/components/collapse/base.js
@@ -9,27 +9,42 @@ $('.js-trigger-collapsed-mobile').on('click', function () {
 
 /**
  * Used by collapsed cms template (Information Page)
+ * @param {int} cmsArrLangLink - get default links of the language switcher
+ * @function cmsLangSwitchHandler - store and update language switcher href depends on what collapse item being clicked
  */
 const cmsCurrentCleanUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+const cmsCollapseTitle = $('.js-collapse-no-tab .collapsed__collapse-title');
+const cmsLangSwitcher = $('.language-selector__link');
 const cmsUrlParamSearch = new Proxy(new URLSearchParams(window.location.search), {
-get: (searchParams, prop) => searchParams.get(prop),
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+const cmsArrLangLink = cmsLangSwitcher.map(function(){
+    return $(this).attr('href');
 });
 var cmsUrlParamValue = cmsUrlParamSearch.contentCollapse;
-const cmsCollapseTitle = $('.js-collapse-no-tab .collapsed__collapse-title');
 
-cmsCollapseTitle.each(function () {
-    const cmsCollapseText = $(this).text();
+function cmsLangSwitchHandler(paramUpdate){
+    cmsLangSwitcher.each(function(index){
+        if (cmsUrlParamValue) {
+            $(this).attr('href', cmsArrLangLink[index] + '?contentCollapse=' + paramUpdate)
+        }
+    });
+}
 
+cmsLangSwitchHandler(cmsUrlParamValue);
+
+cmsCollapseTitle.each(function (cmsIndex) {
     $(this).on('click', function () {
-        window.history.pushState({ path: cmsCurrentCleanUrl }, '', cmsCurrentCleanUrl);
+        window.history.pushState({ path: cmsCurrentCleanUrl }, '', cmsCurrentCleanUrl  + '?contentCollapse=' + cmsIndex);
+
+        cmsLangSwitchHandler(cmsIndex);
+
         $(this).toggleClass('collapsed__collapse-title--active');
         $(this).closest('.collapsed__collapse').find('.collapsed__collapse-content').toggle();
     });
 
     if (cmsUrlParamValue) {
-        const cmsOrigParameterValue = cmsUrlParamValue.replace(/-/g, ' ').toUpperCase();
-
-        if (cmsCollapseText == cmsOrigParameterValue) {
+        if (cmsIndex == cmsUrlParamValue) {
             $(this).addClass('collapsed__collapse-title--active');
             $(this).closest('.collapsed__collapse').find('.collapsed__collapse-content').show();
         }
