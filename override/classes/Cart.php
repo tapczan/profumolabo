3f2a4281c9fae73dac786d02f3fe9cbe2edd2c6e
Aries Meralles <ar.meralles@createit.com>
2022-04-06 17:30:27 +0800

    102 cart product order

---
+++ override/classes/Cart.php
@@ -0,0 +1,246 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */ 
+
+class Cart extends CartCore
+{
+     
+    /**
+     *  
+     */
+    public function getProducts($refresh = false, $id_product = false, $id_country = null, $fullInfos = true, bool $keepOrderPrices = false)
+    {
+        if (!$this->id) {
+            return [];
+        }
+        // Product cache must be strictly compared to NULL, or else an empty cart will add dozens of queries
+        if ($this->_products !== null && !$refresh) {
+            // Return product row with specified ID if it exists
+            if (is_int($id_product)) {
+                foreach ($this->_products as $product) {
+                    if ($product['id_product'] == $id_product) {
+                        return [$product];
+                    }
+                }
+
+                return [];
+            }
+
+            return $this->_products;
+        }
+
+        // Build query
+        $sql = new DbQuery();
+
+        // Build SELECT
+        $sql->select('cp.`id_product_attribute`, cp.`id_product`, cp.`quantity` AS cart_quantity, cp.id_shop, cp.`id_customization`, pl.`name`, p.`is_virtual`,
+                        pl.`description_short`, pl.`available_now`, pl.`available_later`, product_shop.`id_category_default`, p.`id_supplier`,
+                        p.`id_manufacturer`, m.`name` AS manufacturer_name, product_shop.`on_sale`, product_shop.`ecotax`, product_shop.`additional_shipping_cost`,
+                        product_shop.`available_for_order`, product_shop.`show_price`, product_shop.`price`, product_shop.`active`, product_shop.`unity`, product_shop.`unit_price_ratio`,
+                        stock.`quantity` AS quantity_available, p.`width`, p.`height`, p.`depth`, stock.`out_of_stock`, p.`weight`,
+                        p.`available_date`, p.`date_add`, p.`date_upd`, IFNULL(stock.quantity, 0) as quantity, pl.`link_rewrite`, cl.`link_rewrite` AS category,
+                        CONCAT(LPAD(cp.`id_product`, 10, 0), LPAD(IFNULL(cp.`id_product_attribute`, 0), 10, 0), IFNULL(cp.`id_address_delivery`, 0), IFNULL(cp.`id_customization`, 0)) AS unique_id, cp.id_address_delivery,
+                        product_shop.advanced_stock_management, ps.product_supplier_reference supplier_reference');
+
+        // Build FROM
+        $sql->from('cart_product', 'cp');
+
+        // Build JOIN
+        $sql->leftJoin('product', 'p', 'p.`id_product` = cp.`id_product`');
+        $sql->innerJoin('product_shop', 'product_shop', '(product_shop.`id_shop` = cp.`id_shop` AND product_shop.`id_product` = p.`id_product`)');
+        $sql->leftJoin(
+            'product_lang',
+            'pl',
+            'p.`id_product` = pl.`id_product`
+            AND pl.`id_lang` = ' . (int) $this->getAssociatedLanguage()->getId() . Shop::addSqlRestrictionOnLang('pl', 'cp.id_shop')
+        );
+
+        $sql->leftJoin(
+            'category_lang',
+            'cl',
+            'product_shop.`id_category_default` = cl.`id_category`
+            AND cl.`id_lang` = ' . (int) $this->getAssociatedLanguage()->getId() . Shop::addSqlRestrictionOnLang('cl', 'cp.id_shop')
+        );
+
+        $sql->leftJoin('product_supplier', 'ps', 'ps.`id_product` = cp.`id_product` AND ps.`id_product_attribute` = cp.`id_product_attribute` AND ps.`id_supplier` = p.`id_supplier`');
+        $sql->leftJoin('manufacturer', 'm', 'm.`id_manufacturer` = p.`id_manufacturer`');
+
+        // @todo test if everything is ok, then refactorise call of this method
+        $sql->join(Product::sqlStock('cp', 'cp'));
+
+        // Build WHERE clauses
+        $sql->where('cp.`id_cart` = ' . (int) $this->id);
+        if ($id_product) {
+            $sql->where('cp.`id_product` = ' . (int) $id_product);
+        }
+        $sql->where('p.`id_product` IS NOT NULL');
+
+        // Build ORDER BY
+        $sql->orderBy('cp.`date_add` DESC, cp.`id_product`, cp.`id_product_attribute`');
+
+        if (Customization::isFeatureActive()) {
+            $sql->select('cu.`id_customization`, cu.`quantity` AS customization_quantity');
+            $sql->leftJoin(
+                'customization',
+                'cu',
+                'p.`id_product` = cu.`id_product` AND cp.`id_product_attribute` = cu.`id_product_attribute` AND cp.`id_customization` = cu.`id_customization` AND cu.`id_cart` = ' . (int) $this->id
+            );
+            $sql->groupBy('cp.`id_product_attribute`, cp.`id_product`, cp.`id_shop`, cp.`id_customization`');
+        } else {
+            $sql->select('NULL AS customization_quantity, NULL AS id_customization');
+        }
+
+        if (Combination::isFeatureActive()) {
+            $sql->select('
+                product_attribute_shop.`price` AS price_attribute, product_attribute_shop.`ecotax` AS ecotax_attr,
+                IF (IFNULL(pa.`reference`, \'\') = \'\', p.`reference`, pa.`reference`) AS reference,
+                (p.`weight`+ pa.`weight`) weight_attribute,
+                IF (IFNULL(pa.`ean13`, \'\') = \'\', p.`ean13`, pa.`ean13`) AS ean13,
+                IF (IFNULL(pa.`isbn`, \'\') = \'\', p.`isbn`, pa.`isbn`) AS isbn,
+                IF (IFNULL(pa.`upc`, \'\') = \'\', p.`upc`, pa.`upc`) AS upc,
+                IF (IFNULL(pa.`mpn`, \'\') = \'\', p.`mpn`, pa.`mpn`) AS mpn,
+                IFNULL(product_attribute_shop.`minimal_quantity`, product_shop.`minimal_quantity`) as minimal_quantity,
+                IF(product_attribute_shop.wholesale_price > 0,  product_attribute_shop.wholesale_price, product_shop.`wholesale_price`) wholesale_price
+            ');
+
+            $sql->leftJoin('product_attribute', 'pa', 'pa.`id_product_attribute` = cp.`id_product_attribute`');
+            $sql->leftJoin('product_attribute_shop', 'product_attribute_shop', '(product_attribute_shop.`id_shop` = cp.`id_shop` AND product_attribute_shop.`id_product_attribute` = pa.`id_product_attribute`)');
+        } else {
+            $sql->select(
+                'p.`reference` AS reference, p.`ean13`, p.`isbn`,
+                p.`upc` AS upc, p.`mpn` AS mpn, product_shop.`minimal_quantity` AS minimal_quantity, product_shop.`wholesale_price` wholesale_price'
+            );
+        }
+
+        $sql->select('image_shop.`id_image` id_image, il.`legend`');
+        $sql->leftJoin('image_shop', 'image_shop', 'image_shop.`id_product` = p.`id_product` AND image_shop.cover=1 AND image_shop.id_shop=' . (int) $this->id_shop);
+        $sql->leftJoin('image_lang', 'il', 'il.`id_image` = image_shop.`id_image` AND il.`id_lang` = ' . (int) $this->getAssociatedLanguage()->getId());
+
+        $result = Db::getInstance()->executeS($sql);
+
+        // Reset the cache before the following return, or else an empty cart will add dozens of queries
+        $products_ids = [];
+        $pa_ids = [];
+        if ($result) {
+            foreach ($result as $key => $row) {
+                $products_ids[] = $row['id_product'];
+                $pa_ids[] = $row['id_product_attribute'];
+                $specific_price = SpecificPrice::getSpecificPrice($row['id_product'], $this->id_shop, $this->id_currency, $id_country, $this->id_shop_group, $row['cart_quantity'], $row['id_product_attribute'], $this->id_customer, $this->id);
+                if ($specific_price) {
+                    $reduction_type_row = ['reduction_type' => $specific_price['reduction_type']];
+                } else {
+                    $reduction_type_row = ['reduction_type' => 0];
+                }
+
+                $result[$key] = array_merge($row, $reduction_type_row);
+            }
+        }
+        // Thus you can avoid one query per product, because there will be only one query for all the products of the cart
+        Product::cacheProductsFeatures($products_ids);
+        Cart::cacheSomeAttributesLists($pa_ids, (int) $this->getAssociatedLanguage()->getId());
+
+        if (empty($result)) {
+            $this->_products = [];
+
+            return [];
+        }
+
+        if ($fullInfos) {
+            $cart_shop_context = Context::getContext()->cloneContext();
+
+            $givenAwayProductsIds = [];
+
+            if ($this->shouldSplitGiftProductsQuantity && $refresh) {
+                $gifts = $this->getCartRules(CartRule::FILTER_ACTION_GIFT, false);
+                if (count($gifts) > 0) {
+                    foreach ($gifts as $gift) {
+                        foreach ($result as $rowIndex => $row) {
+                            if (!array_key_exists('is_gift', $result[$rowIndex])) {
+                                $result[$rowIndex]['is_gift'] = false;
+                            }
+
+                            if (
+                                $row['id_product'] == $gift['gift_product'] &&
+                                $row['id_product_attribute'] == $gift['gift_product_attribute']
+                            ) {
+                                $row['is_gift'] = true;
+                                $result[$rowIndex] = $row;
+                            }
+                        }
+
+                        $index = $gift['gift_product'] . '-' . $gift['gift_product_attribute'];
+                        if (!array_key_exists($index, $givenAwayProductsIds)) {
+                            $givenAwayProductsIds[$index] = 1;
+                        } else {
+                            ++$givenAwayProductsIds[$index];
+                        }
+                    }
+                }
+            }
+
+            $this->_products = [];
+
+            foreach ($result as &$row) {
+                if (!array_key_exists('is_gift', $row)) {
+                    $row['is_gift'] = false;
+                }
+
+                $additionalRow = Product::getProductProperties((int) $this->id_lang, $row);
+                $row['reduction'] = $additionalRow['reduction'];
+                $row['reduction_without_tax'] = $additionalRow['reduction_without_tax'];
+                $row['price_without_reduction'] = $additionalRow['price_without_reduction'];
+                $row['specific_prices'] = $additionalRow['specific_prices'];
+                unset($additionalRow);
+
+                $givenAwayQuantity = 0;
+                $giftIndex = $row['id_product'] . '-' . $row['id_product_attribute'];
+                if ($row['is_gift'] && array_key_exists($giftIndex, $givenAwayProductsIds)) {
+                    $givenAwayQuantity = $givenAwayProductsIds[$giftIndex];
+                }
+
+                if (!$row['is_gift'] || (int) $row['cart_quantity'] === $givenAwayQuantity) {
+                    $row = $this->applyProductCalculations($row, $cart_shop_context, null, $keepOrderPrices);
+                } else {
+                    // Separate products given away from those manually added to cart
+                    $this->_products[] = $this->applyProductCalculations($row, $cart_shop_context, $givenAwayQuantity, $keepOrderPrices);
+                    unset($row['is_gift']);
+                    $row = $this->applyProductCalculations(
+                        $row,
+                        $cart_shop_context,
+                        $row['cart_quantity'] - $givenAwayQuantity,
+                        $keepOrderPrices
+                    );
+                }
+
+                $this->_products[] = $row;
+            }
+        } else {
+            $this->_products = $result;
+        }
+
+        return $this->_products;
+    }
+ 
+}
