c13b7570490b6b5c7e6a312721358c36790e5170
John Clark Semblante <jc.semblante@createit.com>
2022-04-28 22:46:03 +0800

    157: Updates on header, onas page and main page

--- themes/profumo-labo/_dev/js/components/header/base.js
+++ themes/profumo-labo/_dev/js/components/header/base.js
@@ -19,3 +19,27 @@ $('.header__nav .js-search-input').keyup(function () {
 
     $(".header__inner .js-search-input").val(value);
 });
+
+/**
+ * Header mobile marki collapse
+ */
+$('.mobile-menu__accordion-header').on('click', function(){
+    $('.mobile-menu__accordion-item').stop().removeClass('mobile-menu__accordion-item--active');
+    $(this).closest('.mobile-menu__accordion-item').stop().toggleClass('mobile-menu__accordion-item--active');
+});
+
+
+/**
+ * Smooth scroll on onas/about page
+ */
+function smoothScrollingTo(target){
+    $('html,body').animate({scrollTop:$(target).offset().top - 50}, 500);
+}
+
+$('.mega-menu-header-about a[href*="#"]').on('click', function(){  
+    smoothScrollingTo(this.hash);
+});
+
+$(window).on('load', function(){
+    smoothScrollingTo(location.hash);
+});
