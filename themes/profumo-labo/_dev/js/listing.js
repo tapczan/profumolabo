2703abcc58e5fb0a37ad9ccb7c29b2c100c702c6
Aries Meralles <ar.meralles@createit.com>
2022-06-14 21:15:25 +0800

    274 navigation filter

--- themes/profumo-labo/_dev/js/listing.js
+++ themes/profumo-labo/_dev/js/listing.js
@@ -42,17 +42,37 @@ function checkFilteredCategory(data) {
 
   let newData = [],
       filteredCategory = {
-        // "category-id-3" : '.manufacturer',
-        "category-id-170" : '.manufacturer',
-        "category-id-166" : '.manufacturer',
-        "category-id-36"  : '.manufacturer',
-        "category-id-46"  : '.manufacturer',
-        "category-id-54"  : '.manufacturer',
-        "category-id-157" : '.manufacturer',
-        "category-id-79"  : '.manufacturer',
-        "category-id-89"  : '.manufacturer',
-        "category-id-107" : '.manufacturer',
-        "category-id-115" : '.manufacturer'
+        "category-id-164" : '.feature_7', // open bestsellers
+        "category-id-34"  : '.feature_7', // ""
+        "category-id-44"  : '.feature_7', // ""
+        "category-id-52"  : '.feature_7', // ""
+        "category-id-155" : '.feature_7', // ""
+        "category-id-77"  : '.feature_7', // ""
+        "category-id-87"  : '.feature_7', // ""
+        "category-id-105" : '.feature_7', // ""
+        "category-id-113" : '.feature_7', // ""
+        "category-id-166" : '.manufacturer', // open brand inspiration
+        "category-id-36"  : '.manufacturer', // ""
+        "category-id-46"  : '.manufacturer', // ""
+        "category-id-54"  : '.manufacturer', // ""
+        "category-id-157" : '.manufacturer', // ""
+        "category-id-79"  : '.manufacturer', // ""
+        "category-id-89"  : '.manufacturer', // ""
+        "category-id-107" : '.manufacturer', // ""
+        "category-id-115" : '.manufacturer', // ""
+        "category-id-170" : '.feature_1', // open fragrance group
+        "category-id-83"  : '.feature_1', // ""
+        "category-id-173" : '.feature_5', // open unisex
+        "category-id-41"  : '.feature_5', // ""
+        "category-id-51"  : '.feature_5', // ""
+        "category-id-59"  : '.feature_5', // ""
+        "category-id-162" : '.feature_5', // ""
+        "category-id-86"  : '.feature_5', // ""
+        "category-id-94"  : '.feature_5', // ""
+        "category-id-112" : '.feature_5', // ""
+        "category-id-121" : '.feature_5', // ""
+        "category-id-169" : '.feature_2', // open time of year
+        "category-id-82" : '.feature_2', // ""
       };
 
   newData = Object.entries(data.page.body_classes);
