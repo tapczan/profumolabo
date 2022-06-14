a800f9c0d546e8be8725c19b9676448b1b4a9f09
Aries Meralles <ar.meralles@createit.com>
2022-06-14 19:28:12 +0800

    274 navigation + filter

--- themes/profumo-labo/_dev/js/listing.js
+++ themes/profumo-labo/_dev/js/listing.js
@@ -6,6 +6,8 @@ $(() => {
   /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "filters" }] */
   const filters = new Filters();
 
+  // console.log(filters);
+
   prestashop.on('updateProductList', (data) => {
     updateProductListDOM(data);
     window.scrollTo(0, 0);
@@ -14,7 +16,38 @@ $(() => {
   prestashop.on('updatedProductList', () => {
     prestashop.pageLazyLoad.update();
   });
+
+  if(prestashop.page.body_classes['page-category']) {
+    console.log('i am category')
+    checkFilteredCategory(prestashop);
+  }
+  
 });
+
+function checkFilteredCategory(data) {
+
+  let newData = [],
+      filteredCategory = [
+        ['category-id-170','.manufacturer'],
+        ['category-id-166','.manufacturer'],
+        ['category-id-36','.manufacturer'],
+        ['category-id-46','.manufacturer'],
+        ['category-id-54','.manufacturer'],
+        ['category-id-157','.manufacturer'],
+        ['category-id-79','.manufacturer'],
+        ['category-id-89','.manufacturer'],
+        ['category-id-107','.manufacturer'],
+        ['category-id-115','.manufacturer']
+      ];
+
+  newData = Object.entries(data.page.body_classes);
+
+  newData.forEach(function(newdata) {
+    if( filteredCategory[0][0].includes(newdata[0]) ) {
+      activateFilter(filteredCategory[0][1]);
+    }
+  });
+}
 
 
 function updateProductListDOM(data) {
@@ -29,4 +62,15 @@ function updateProductListDOM(data) {
 
   prestashop.customSelect.init();
   prestashop.emit('updatedProductList', data);
+
+}
+
+function activateFilter(facetType) {
+ 
+  $('.filter-top-show').addClass('open');
+  $('.js-filter-wrapper').addClass('filter-wrapper--show');
+  $('.js-listing-wrapper').removeClass('listing-wrapper--default');
+
+  $('a'+facetType).attr('aria-expanded', true);
+  $('div'+facetType).addClass('show');
 }
