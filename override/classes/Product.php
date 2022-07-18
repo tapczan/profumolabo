f5c7f824e1c233fe4df18d2f19e3d4f6479eba3d
Angelo Dan F. Bagasbas <a.bagasbas@createit.com>
2022-07-18 20:58:03 +0800

    #331 - removed drugstore products

--- override/classes/Product.php
+++ override/classes/Product.php
@@ -314,6 +314,174 @@ class Product extends ProductCore
         return self::$productPropertiesCache[$cache_key];
     }
 
+    /**
+     * @param int $id_lang
+     * @param int $page_number
+     * @param int $nb_products
+     * @param false $count
+     * @param null $order_by
+     * @param null $order_way
+     * @param false $beginning
+     * @param false $ending
+     * @param Context|null $context
+     * @return array|false|string|void
+     * @throws PrestaShopDatabaseException
+     * @throws PrestaShopException
+     */
+    public static function getPricesDrop(
+        $id_lang,
+        $page_number = 0,
+        $nb_products = 10,
+        $count = false,
+        $order_by = null,
+        $order_way = null,
+        $beginning = false,
+        $ending = false,
+        Context $context = null
+    ) {
+        if (!Validate::isBool($count)) {
+            die(Tools::displayError());
+        }
+
+        if (!$context) {
+            $context = Context::getContext();
+        }
+        if ($page_number < 1) {
+            $page_number = 1;
+        }
+        if ($nb_products < 1) {
+            $nb_products = 10;
+        }
+        if (empty($order_by) || $order_by == 'position') {
+            $order_by = 'price';
+        }
+        if (empty($order_way)) {
+            $order_way = 'DESC';
+        }
+        if ($order_by == 'id_product' || $order_by == 'price' || $order_by == 'date_add' || $order_by == 'date_upd') {
+            $order_by_prefix = 'product_shop';
+        } elseif ($order_by == 'name') {
+            $order_by_prefix = 'pl';
+        }
+        if (!Validate::isOrderBy($order_by) || !Validate::isOrderWay($order_way)) {
+            die(Tools::displayError());
+        }
+        $current_date = date('Y-m-d H:i:00');
+        $ids_product = Product::_getProductIdByDate((!$beginning ? $current_date : $beginning), (!$ending ? $current_date : $ending), $context);
+
+        $tab_id_product = [];
+        foreach ($ids_product as $product) {
+            if (is_array($product)) {
+                $tab_id_product[] = (int) $product['id_product'];
+            } else {
+                $tab_id_product[] = (int) $product;
+            }
+        }
+
+        $front = true;
+        if (!in_array($context->controller->controller_type, ['front', 'modulefront'])) {
+            $front = false;
+        }
+
+        $sql_groups = '';
+        if (Group::isFeatureActive()) {
+            $groups = FrontController::getCurrentCustomerGroups();
+            $sql_groups = ' AND EXISTS(SELECT 1 FROM `' . _DB_PREFIX_ . 'category_product` cp
+            JOIN `' . _DB_PREFIX_ . 'category_group` cg ON (cp.id_category = cg.id_category AND cg.`id_group` ' . (count($groups) ? 'IN (' . implode(',', $groups) . ')' : '=' . (int) Group::getCurrent()->id) . ')
+            WHERE cp.`id_product` = p.`id_product`)';
+        }
+
+
+        $excludedProductsFromCatQuery = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
+            SELECT id_product FROM ' . _DB_PREFIX_ . 'category_product where id_category = 140;
+        ');
+
+        $excludedProductsId = [];
+
+        if($excludedProductsFromCatQuery) {
+            foreach ($excludedProductsFromCatQuery as $category) {
+                $excludedProductsId[] = $category['id_product'];
+            }
+        }
+
+        $excludedProductsString = implode(',', $excludedProductsId);
+        $excludedProductsQuery = '';
+
+        if(!empty($excludedProductsString)){
+            $excludedProductsQuery = 'AND p.`id_product` NOT IN ('.$excludedProductsString.')';
+        }
+
+        if ($count) {
+            return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
+            SELECT COUNT(DISTINCT p.`id_product`)
+            FROM `' . _DB_PREFIX_ . 'product` p
+            ' . Shop::addSqlAssociation('product', 'p') . '
+            WHERE product_shop.`active` = 1
+            AND product_shop.`show_price` = 1
+            ' . ($front ? ' AND product_shop.`visibility` IN ("both", "catalog")' : '') . '
+            ' . ((!$beginning && !$ending) ? 'AND p.`id_product` IN(' . ((is_array($tab_id_product) && count($tab_id_product)) ? implode(', ', $tab_id_product) : 0) . ')' : '') . '
+            ' . $sql_groups);
+        }
+
+        if (strpos($order_by, '.') > 0) {
+            $order_by = explode('.', $order_by);
+            $order_by = pSQL($order_by[0]) . '.`' . pSQL($order_by[1]) . '`';
+        }
+
+        $sql = '
+        SELECT
+            p.*, product_shop.*, stock.out_of_stock, IFNULL(stock.quantity, 0) as quantity, pl.`description`, pl.`description_short`, pl.`available_now`, pl.`available_later`,
+            IFNULL(product_attribute_shop.id_product_attribute, 0) id_product_attribute,
+            pl.`link_rewrite`, pl.`meta_description`, pl.`meta_keywords`, pl.`meta_title`,
+            pl.`name`, image_shop.`id_image` id_image, il.`legend`, m.`name` AS manufacturer_name,
+            DATEDIFF(
+                p.`date_add`,
+                DATE_SUB(
+                    "' . date('Y-m-d') . ' 00:00:00",
+                    INTERVAL ' . (Validate::isUnsignedInt(Configuration::get('PS_NB_DAYS_NEW_PRODUCT')) ? Configuration::get('PS_NB_DAYS_NEW_PRODUCT') : 20) . ' DAY
+                )
+            ) > 0 AS new
+        FROM `' . _DB_PREFIX_ . 'product` p
+        ' . Shop::addSqlAssociation('product', 'p') . '
+        LEFT JOIN `' . _DB_PREFIX_ . 'product_attribute_shop` product_attribute_shop
+            ON (p.`id_product` = product_attribute_shop.`id_product` AND product_attribute_shop.`default_on` = 1 AND product_attribute_shop.id_shop=' . (int) $context->shop->id . ')
+        ' . Product::sqlStock('p', 0, false, $context->shop) . '
+        LEFT JOIN `' . _DB_PREFIX_ . 'product_lang` pl ON (
+            p.`id_product` = pl.`id_product`
+            AND pl.`id_lang` = ' . (int) $id_lang . Shop::addSqlRestrictionOnLang('pl') . '
+        )
+        LEFT JOIN `' . _DB_PREFIX_ . 'image_shop` image_shop
+            ON (image_shop.`id_product` = p.`id_product` AND image_shop.cover=1 AND image_shop.id_shop=' . (int) $context->shop->id . ')
+        LEFT JOIN `' . _DB_PREFIX_ . 'image_lang` il ON (image_shop.`id_image` = il.`id_image` AND il.`id_lang` = ' . (int) $id_lang . ')
+        LEFT JOIN `' . _DB_PREFIX_ . 'manufacturer` m ON (m.`id_manufacturer` = p.`id_manufacturer`)
+        WHERE product_shop.`active` = 1
+        AND product_shop.`show_price` = 1
+        ' . ($front ? ' AND product_shop.`visibility` IN ("both", "catalog")' : '') . '
+        ' . ((!$beginning && !$ending) ? ' AND p.`id_product` IN (' . ((is_array($tab_id_product) && count($tab_id_product)) ? implode(', ', $tab_id_product) : 0) . ')' : '') . '
+        ' . $sql_groups . '
+        ' . $excludedProductsQuery;
+
+        if ($order_by != 'price') {
+            $sql .= '
+				ORDER BY ' . (isset($order_by_prefix) ? pSQL($order_by_prefix) . '.' : '') . pSQL($order_by) . ' ' . pSQL($order_way) . '
+				LIMIT ' . (int) (($page_number - 1) * $nb_products) . ', ' . (int) $nb_products;
+        }
+
+        $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
+
+
+        if (!$result) {
+            return false;
+        }
+
+        if ($order_by === 'price') {
+            Tools::orderbyPrice($result, $order_way);
+            $result = array_slice($result, (int) (($page_number - 1) * $nb_products), (int) $nb_products);
+        }
+
+        return Product::getProductsProperties($id_lang, $result);
+    }
+
     public function getProductAttributeCombinationByGroup($id_lang, $id_product, $attr_group)
     {
         if (!Combination::isFeatureActive()) {
